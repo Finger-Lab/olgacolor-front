d08c95931c9e7a7f270b83707e93df7a10891718
RamonRossini <ramonhbonfim@gmail.com>
2025-08-05 20:38:50 -0300

    refactor(footer): enhance dropdown functionality and improve HTML structure, remove obsolete test file

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 
 @Component({
@@ -7,12 +7,33 @@ import { Router, RouterLink } from '@angular/router';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {  
-  currentYear = new Date().getFullYear()
+export class FooterComponent {
 
-  constructor(private router: Router) {}
+  private router = inject(Router);
 
-  onPerfilClick(category: string) {
-    this.router.navigate(['/produtos'], { queryParams: { category: category.toUpperCase() } })
+  protected currentYear = new Date().getFullYear();
+
+  protected onPerfilClick(category: string) {
+    this.router.navigate(['/produtos'], { queryParams: { category: category.toUpperCase() } });
   }
+
+  protected onFinishesClick(category: string): void {
+    this.router.navigate(['/acabamentos'], { queryParams: { category: category.toUpperCase() } })
+  }
+
+  // Métodos para controlar o hover dos dropdowns
+  protected onDropdownMouseEnter(event: Event): void {
+    const target = event.currentTarget as HTMLElement;
+    const dropdownMenu = target.querySelector('.dropdown-menu') as HTMLElement;
+    if (dropdownMenu)
+      dropdownMenu.classList.add('show');
+  }
+
+  protected onDropdownMouseLeave(event: Event): void {
+    const target = event.currentTarget as HTMLElement;
+    const dropdownMenu = target.querySelector('.dropdown-menu') as HTMLElement;
+    if (dropdownMenu)
+      dropdownMenu.classList.remove('show');
+  }
+
 }
