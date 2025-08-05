daf2c89be6dc20e9d9bc262a0d149b4054b0a1a6
RamonRossini <ramonhbonfim@gmail.com>
2025-08-04 23:17:57 -0300

    refactor(products): convert dynamic properties to signals and update HTML for improved functionality

--- src/app/components/home/products/products.component.ts
+++ src/app/components/home/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { DividingLineComponent } from "../../dividing-line/dividing-line.component";
 import { RouterLink } from '@angular/router';
 
@@ -9,16 +9,16 @@ import { RouterLink } from '@angular/router';
   styleUrl: './products.component.scss'
 })
 export class ProductsComponent {
-  dynamicWidth: number = 10
-  dynamicBg: string = '#000'
 
-  products_blocks = [
+  protected dynamicWidth = signal<number>(10);
+  protected dynamicBg = signal<string>('#000');
+  protected products_blocks = signal<any[]>([
     {
       imgSrc: 'assets/images/home/aglo.avif',
       altImg: 'Logo da aglo',
       details: [
-        { value: '28 dB', description: 'Porta de correr 02 folhas'},
-        { value: '31 dB', description: 'para carregamento total'}
+        { value: '28 dB', description: 'Porta de correr 02 folhas' },
+        { value: '31 dB', description: 'para carregamento total' }
       ],
       description: 'Sistemas residenciais e comerciais completos para todos os padrões: alto, médio e popular. Atendem plenamente às normas nas cinco regiões do país.',
       productImg: 'assets/images/home/products_aglo.avif',
@@ -30,8 +30,8 @@ export class ProductsComponent {
       imgSrc: 'assets/images/home/lock.avif',
       altImg: 'Logo da Lock',
       details: [
-        { value: '39 dB', description: 'Sistema residencial acústico'},
-        { value: '45mm', description: 'Perfis de Bitola'}
+        { value: '39 dB', description: 'Sistema residencial acústico' },
+        { value: '45mm', description: 'Perfis de Bitola' }
       ],
       description: 'Sistema de atenuação acústica para portas e janelas deslizantes e de giro, que reduzem até 39dB os ruídos característicos dos grandes centros urbanos. Ultrapassam os desempenhos exigidos pelas normas brasileiras e garantem além do isolamento sonoro, estanqueidade ao ar, água e ao vento.',
       productImg: 'assets/images/home/products_lock.avif',
@@ -43,8 +43,8 @@ export class ProductsComponent {
       imgSrc: 'assets/images/home/grid.avif',
       altImg: 'Logo da Grid',
       details: [
-        { value: '90°', description: 'Sistema construtivo para colunas'},
-        { value: '45°', description: 'Equadrações do maxim-ar'}
+        { value: '90°', description: 'Sistema construtivo para colunas' },
+        { value: '45°', description: 'Equadrações do maxim-ar' }
       ],
       description: 'Sistema construtivo para fachadas cortina e entre vãos. Os perfis de alumínio se sobrepõem externamente criando formas geométricas planejadas com forte apelo estético. Sistema construtivo à 90˚para colunas, ancoragens, travessas, presilhas e tampas, sendo as requadrações do maxim-ar à 45˚, gerando excelente produtividade na fabricação e instalação.',
       productImg: 'assets/images/home/products_grid.avif',
@@ -52,5 +52,6 @@ export class ProductsComponent {
       isLeft: true,
       isLast: true
     },
-  ]
+  ]);
+
 }
