1f5164a0a64adba135ef8406c79c074166724954
RamonRossini <ramonhbonfim@gmail.com>
2025-08-04 23:21:23 -0300

    refactor(price): convert dynamic properties to signals, enhance HTML structure, and remove obsolete test file

--- src/app/components/home/price/price.component.ts
+++ src/app/components/home/price/price.component.ts
@@ -1,7 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { DividingLineComponent } from "../../dividing-line/dividing-line.component";
 import { ApiResponse, PriceService } from '../../../services/price.service';
 import { CommonModule } from '@angular/common';
+import { catchError, filter, of } from 'rxjs';
 
 @Component({
   selector: 'app-price',
@@ -9,47 +10,55 @@ import { CommonModule } from '@angular/common';
   templateUrl: './price.component.html',
   styleUrl: './price.component.scss'
 })
-export class PriceComponent {
-  dynamicWidth: number = 10
-  dynamicBg: string = '#000'
-  currencies: any[] = []
-  currentMonth: number
-  currentYear: number
+export class PriceComponent implements OnInit {
 
-  constructor(
-    private priceService: PriceService
-  ) { }
+  protected dynamicWidth = signal<number>(10);
+  protected dynamicBg = signal<string>('#000');
+  protected currencies = signal<any[]>([]);
+  protected currentMonth = signal<number>(0);
+  protected currentYear = signal<number>(0);
+
+  private priceService = inject(PriceService);
 
   ngOnInit(): void {
-    const currentDate = new Date()
-    const currentMonth = currentDate.getMonth()
-    const currentYear = currentDate.getFullYear()
-    this.currentMonth = currentMonth + 1
-    this.currentYear = currentYear
+    try {
 
-    this.priceService.getCurrencies().subscribe({
-      next: (res: ApiResponse) => {
-        const results = res.results
-        const currencies = results.currencies
+      const currentDate = new Date();
+      const currentMonth = currentDate.getMonth();
+      const currentYear = currentDate.getFullYear();
+      this.currentMonth.set(currentMonth + 1);
+      this.currentYear.set(currentYear);
 
-        const currencyKeys = Object.keys(currencies)
+      this.priceService.getCurrencies().pipe(
+        catchError(_ => {
+          console.error('Erro ao buscar cotação LME')
+          return of(null)
+        }),
+        filter(response => !!response?.results?.currencies),
+      ).subscribe((res: ApiResponse | null) => {
+        if (!res) return
+
+        const results = res.results;
+        const currencies = results.currencies;
+
+        const currencyKeys = Object.keys(currencies);
 
         for (let i = 1; i < 5; i++) {
           const key = currencyKeys[i]
           const currency = currencies[key]
 
-          this.currencies.push({
+          this.currencies.update(prev => [...prev, {
             sigla: key,
             buy: currency?.buy,
             name: currency.name,
             code: currency.code,
             variation: currency.variation
-          })
-        }
-      },
-      error: (err) => {
-        console.error(err)
-      }
-    })
+          }])
+        };
+      })
+    } catch (error) {
+      console.log('Erro ao buscar cotação LME', error)
+    }
   }
+
 }
--- src/app/services/price.service.ts
+++ src/app/services/price.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 export interface ApiResponse {
@@ -20,14 +20,13 @@ export interface Currency {
   providedIn: 'root'
 })
 export class PriceService {
-  // private apiUrl = 'https://api.hgbrasil.com/finance?format=json-cors&key=b99232a3'
-  private apiUrl = 'https://api.hgbrasil.com/finance?format=json-cors'
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  // private apiUrl = 'https://api.hgbrasil.com/finance?format=json-cors&key=b99232a3';
+  private apiUrl: string = 'https://api.hgbrasil.com/finance?format=json-cors';
+  private _http = inject(HttpClient);
 
   getCurrencies(): Observable<ApiResponse> {
-    return this.http.get<ApiResponse>(this.apiUrl)
+    return this._http.get<ApiResponse>(this.apiUrl)
   }
+
 }
