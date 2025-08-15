d869263d7dbae3a443acf6eece39c152f1b1e863
RamonRossini <ramonhbonfim@gmail.com>
2025-08-15 03:05:53 -0300

    refactor(civil-construction): enhance HTML structure, update image links, and remove obsolete test files

--- src/app/pages/civil-construction/civil-construction.component.ts
+++ src/app/pages/civil-construction/civil-construction.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { MainComponent } from "../../../app/components/civil-construction/main/main.component";
-import { NewsletterComponent } from "../../components/newsletter/newsletter.component";
 import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-civil-construction',
-  imports: [HeaderComponent, MainComponent, NewsletterComponent, FooterComponent],
+  imports: [HeaderComponent, MainComponent, FooterComponent],
   templateUrl: './civil-construction.component.html',
   styleUrl: './civil-construction.component.scss'
 })
